5ca33233bb6dd93a1d30db8ab16bb696b071fca3
Matt Zager <mzager09@gmail.com>
2016-11-20 14:57:25 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,157 @@
+//create a way to play again
+//Model code
+
+$(document).ready(function(){
+	$('#container').css({opacity: 0, display: 'flex'}).animate({opacity: 1}, 1000);
+});
+
+/*
+function Evildoer(name) {
+this.name = name;
+}
+
+var darthtrump = new Evildoer("Darth Trump");
+var darthpence = new Evildoer("Darth Pence");
+var christiethehutt = new Evildoer("Christie the Hutt");
+
+var princess = new Object();
+princess.name = "Princess Hillarleia";
+princess.free = false;
+
+var voter = new Object();
+
+//Evaluates whether you beat Pence and freed princess//
+darthtrump.showdown = function() {
+if (princess.free === true){
+	alert("Darth Trump appears breathing heavily (He just walked up 48 flights of steps because the elevators broke at Trump Towers). He issues a challenge: Convince a Trump supporter of his choosing to switch to Hillarleia, and he will drop out of the election. Click OK or hit return to begin.");
+	darthtrump.finalchallenge();
+}
+	else {
+var random = Math.floor((Math.random() * 10) + 1);
+if (random < 6) {
+	alert("Darth Trump appears breathing heavily (He just walked up 48 flights of steps because the elevators broke at Trump Towers). He issues a challenge: Convince a Trump supporter of his choosing to switch to Hillarleia, and he will drop out of the election. He also will set the princess free. Click OK or hit return to begin. ");
+	darthtrump.finalchallenge();
+}
+else {
+	alert("Darth Trump appears breathing heavily (He just walked up 48 flights of steps because the elevators broke at Trump Towers). He chooses not to free the princess, even if you convince a Trump supporter not to vote for him. So you lose. GAME OVER. ")
+	}
+	
+};
+};
+
+//Executes the final challenge, calls the decides method
+darthtrump.finalchallenge = function() {
+	var argument = prompt("Trump plucks a supporter from the audience for the challenge. Now pick an argument to pursuade him to change his vote.\n A) Trump wants to pursue a trade policy that veers from globalization despite warnings from economists. \n B) Trump suggests using stop-and-frisk policing to address crime in cities despite it being ruled unconstitutional. \n C) Trump made fun of a beauty contestant.\n Choose A, B, or C")
+if (argument === "A" || argument === "a" || argument === "B" || argument === "b" || argument === "C" || argument === "c") {
+	if (voter.decides()) {
+		alert("The Trump supporter likes your argument. YOU WIN!!!!");
+		newGame();
+		}
+	else {
+		alert("Once a Trumper, always a Trumper. YOU LOSE!!!");
+		newGame();
+	}
+		}
+else {
+	alert("Please enter A, B, or C without characters or spaces, Young Jedi.");
+	darthtrump.finalchallenge();
+	}
+	};
+
+//Decides whether you convince voter switch
+voter.decides = function() {
+	var decider = Math.floor((Math.random() * 10) + 1);
+	if (decider > 5){
+		return true;
+	}
+	else {
+		return false;
+	}
+	
+};
+
+
+//Challenges you to fight Darth Pence
+darthpence.fightsyou = function() {
+	var fightdarthpence = prompt("You have entered the hideout of Christie the Hutt. Christie issues a challenge: He will release Princess Hillarleia if you win a light-saber fight with Darth Pence. If you opt not to fight, Darth Trump could choose not to challenge you later, since the princess isn't free, and you will automatically lose.\nWill you fight Darth Pence? Answer Y or N.");
+	var fighting = true;
+	if (fightdarthpence === "Y" || fightdarthpence === "y"){
+	while (fighting){
+		var number = Math.floor((Math.random() * 10) + 1);
+		if (number < 4){
+			alert("Darth Pence kills you. Darth Trump wins the election. Congratulations, you let down the universe. Click OK, even though you can't because you're dead.");
+			fighting = false;
+			newGame();
+		}
+		else if (number > 3  && number < 8){
+			alert("Congratulations, you killed Darth Pence.");
+			princess.free = true;
+			fighting = false;
+			darthtrump.showdown(); 
+		}
+		else {   
+			var fightprogresses = true;
+			while(fightprogresses){
+			var keepplaying = prompt("Pence continues to put up a tough fight. You can retreat, although the princess won't go free, decreasing your chances of defeating Darth Trump later. Do you want to keep fighting? Answer Y or N.");
+			if(keepplaying === "Y" || keepplaying === "y") {
+				fightprogresses = false;
+				alert("You are brave. Click OK and fight on.");
+			}
+			else if (keepplaying === "n" || keepplaying === "N") {
+				fightprogresses = false;
+				alert("You have potentially let down the rebellion. Continue on to attempt to defeat Darth Trump. Click OK.");
+				fighting = false;
+				darthtrump.showdown();
+			}
+			else {
+				alert("You must answer Y or N.")
+				}
+		}
+		
+		}
+	}	
+	}
+	
+	else if (fightdarthpence === "N" || fightdarthpence === "n"){
+		alert("You have potentially let down the rebellion. Continue on to attempt to defeat Darth Trump. Click OK.");
+		fighting = false;
+		
+		darthtrump.showdown();
+	}
+
+	else {
+		alert("You must answer Y or no. N.");
+		darthpence.fightsyou();
+	}
+
+	};
+
+
+
+	
+
+
+//Controller code
+
+function newGame () {
+	var agreetoplay = prompt("A long time ago in a galaxy far far away ... \nDarth Trump threatened to smash the rebellion and rule the galaxy if he got elected. His second in command, Darth Pence, along with Christie the Hutt, kidnapped Princess Hillarleia. Your mission is to rescue the princess and convince a voter not to support Darth Trump, saving the galaxy from his evil clutches. Do you accept this mission? Answer Y or N.");
+	
+	//START HERE: Check if you need !=null, also remove unnecessary code above
+	if (agreetoplay === "Y" ||  agreetoplay === "y") {
+	darthpence.fightsyou();
+	}
+	else if (agreetoplay === "n" || agreetoplay === "N") {
+	alert("OK, Click refresh when you are ready, Young Jedi.");
+	}
+	else {
+	alert("You must answer Y or N. Try again.")
+	newGame();
+	}
+
+};
+
+
+newGame();
+*/
+
+
