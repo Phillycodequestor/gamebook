d588274203a85a95039666eb33b15397d473182b
Matt Zager <mzager09@gmail.com>
2017-02-16 13:00:05 -0500

    'commit10'

--- js/main.js
+++ js/main.js
@@ -1,65 +1,37 @@
 /*
-var christiethehutt = new Evildoer("Christie the Hutt");
-
 var princess = new Object();
 princess.name = "Princess Hillarleia";
 princess.free = false;
 
-var voter = new Object();
+
 
 //Evaluates whether you beat Pence and freed princess//
 darthtrump.showdown = function() {
 if (princess.free === true){
-	alert("Darth Trump appears breathing heavily (He just walked up 48 flights of steps because the elevators broke at Trump Towers). He issues a challenge: Convince a Trump supporter of his choosing to switch to Hillarleia, and he will drop out of the election. Click OK or hit return to begin.");
-	darthtrump.finalchallenge();
-}
-	else {
-var random = Math.floor((Math.random() * 10) + 1);
-if (random < 6) {
-	alert("Darth Trump appears breathing heavily (He just walked up 48 flights of steps because the elevators broke at Trump Towers). He issues a challenge: Convince a Trump supporter of his choosing to switch to Hillarleia, and he will drop out of the election. He also will set the princess free. Click OK or hit return to begin. ");
-	darthtrump.finalchallenge();
+darthtrump.finalchallenge();
 }
 else {
-	alert("Darth Trump appears breathing heavily (He just walked up 48 flights of steps because the elevators broke at Trump Towers). He chooses not to free the princess, even if you convince a Trump supporter not to vote for him. So you lose. GAME OVER. ")
-	}
-	
-};
+Call randomizer here to determine if game will continue
+}
 };
 
 //Executes the final challenge, calls the decides method
 darthtrump.finalchallenge = function() {
-	var argument = prompt("Trump plucks a supporter from the audience for the challenge. Now pick an argument to pursuade him to change his vote.\n A) Trump wants to pursue a trade policy that veers from globalization despite warnings from economists. \n B) Trump suggests using stop-and-frisk policing to address crime in cities despite it being ruled unconstitutional. \n C) Trump made fun of a beauty contestant.\n Choose A, B, or C")
-if (argument === "A" || argument === "a" || argument === "B" || argument === "b" || argument === "C" || argument === "c") {
-	if (voter.decides()) {
-		alert("The Trump supporter likes your argument. YOU WIN!!!!");
-		newGame();
-		}
-	else {
-		alert("Once a Trumper, always a Trumper. YOU LOSE!!!");
-		newGame();
-	}
-		}
-else {
-	alert("Please enter A, B, or C without characters or spaces, Young Jedi.");
-	darthtrump.finalchallenge();
-	}
+May or may not need this
 	};
-
-//Decides whether you convince voter switch
-voter.decides = function() {
-	var decider = Math.floor((Math.random() * 10) + 1);
-	if (decider > 5){
-		return true;
-	}
-	else {
-		return false;
-	}
-	
-};
 
 */
  
 $(document).ready(function(){
+//Recalls fightoutcome if you don't retreat
+$("#radio5").click(function(){
+$('#text5').hide();
+fightoutcome();
+});
+$("#radio6").click(function(){
+$('#text5').hide();
+$('#text6').fadeIn(4000);
+});
 
 function Evildoer(name) {
 this.name = name;
@@ -113,40 +85,25 @@ alert("OK, Click refresh when you are ready, Young Jedi.");
 
 //calls randomizer, tells you if you won, lost, have option to retreat (calls fightorflight())
 var fightoutcome = function(){
-
 document.getElementById("radio5").checked = false;
-/*
 var number = randomizer();
 //PLAYER LOSES DUEL
 if (number < 4){
-keepfighting = false;
 playerloses();
 }
 //PLAYER WINS DUEL
 else if (number > 3  && number < 8) {
 $('#text4').fadeIn(4000);
 //princess.free = true;
-keepfighting = false;
 $('#button').click(function(){
-$('#text4').hide("explode");
+$('#text4').hide("explode", {pieces: 32}, 2000);
 //darthtrump.showdown();
 });
 }
-*/
 //PLAYER GIVEN OPTION TO RETREAT
-//else {
+else {
 $('#text5').fadeIn(4000);
-
-//}
-$("#radio5").click(function(){
-$('#text5').hide();
-//$("[id=radio5]").removeAttr("checked");
-fightoutcome();
-});
-$("#radio6").click(function(){
-$('#text5').hide();
-$('#text6').fadeIn(4000);
-});
+}
 };
 
 //darthtrump.showdown = function(){};
@@ -158,19 +115,6 @@ return Math.floor((Math.random() * 10) + 1);
 var playerloses = function() {
 $('#text3').fadeIn(4000);
 };
-
-//returns true or false to fightoutcome based on whether player chooses to keep fighting
-var fightorflight = function() {
-if(radio('radio5')){
-$('#text5').hide();
-return true;
-}
-else {
-$('#text5').hide();
-$('#text6').fadeIn(4000);
-return false;
-}
-}; 
 
 newGame();
 });
