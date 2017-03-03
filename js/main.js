87d8fdfa2cb5f50d98aa3664c8d08d500448eda8
Matt Zager <mzager09@gmail.com>
2017-03-02 19:46:25 -0500

    'commitnow'

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
 $(document).ready(function(){
-var sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, mi nec convallis sollicitudin, turpis arcu maximus odio, at gravida odio lorem id lacus. Etiam facilisis a lorem vel blandit. Cras consequat massa odio, non sagittis diam fringilla sit amet."
 var openingText = "Choose whether the following statements made by Donald Trump are true or false. Two right out of three wins."
 document.getElementById("text1").innerHTML = openingText;
 $('#button1').css({display: "block"});
 
-//START stop button from exit stage left
 $('#button1').click(function(){
-document.getElementById("text1").innerHTML = sampleText;
+document.getElementById("text1").innerHTML = statement[1].text;
 $(this).hide(function(){
 	$('#button2, #button3').css({display:"block"});
 });
 });
+
+//constructor for statement objects
+function falseStatement(text,answer){
+this.text = text;
+this.answer = answer;
+this.accuracy = false;
+};
+
+var statement = [];
+
+statement[0] = new falseStatement("'As secretary of state, Hillary Clinton allowed thousands of criminal aliens to be released because their home countries wouldn’t take them back.' ... 10.21.2016", "A 2001 Supreme Court decision required these people to be released if their home countries wouldn’t take them back. While some critics believe the Bush and Obama administration should have done more to pressure these countries, it wasn’t Clinton’s optional decision to release them.");
+
+statement[1] = new falseStatement("Hillary Clinton laughed at a 12-year-old rape victim: 'Her client she represented got him off and she's seen laughing at the girl who was raped.'","Clinton laughed at various points of a 1980s interview about the case – but never at the victim. For example, she said that her client had passed a polygraph lie test, and she added, with a laugh, 'which forever destroyed my faith in polygraphs.'");
+
+//START create 6 false states and put in array
+
 //this code sends a Question object to the fact function or alt fact function when button clicked to tell if question was fact or alt fact
 /*
 $("#radio7").click(function(){
 fact(qUestion1);
 });
-
-function Question(accuracy, text, answer){
-this.accuracy = accuracy;
-this.text = text;
-this.answer = answer;
-};
-
-var trueQuestions = new Array();
-trueQuestions[0] = new Question(true, "some text", "some text");
-//use this line of code to insert text in the question box
-
 
 //Tests whether question player answered true to actually is true
 var fact = function(question){
@@ -44,7 +47,7 @@ var randomizer = function() {
 return Math.floor((Math.random() * 10) + 1);
 };
 
-newGame();
+//newGame();
 });
 
 
