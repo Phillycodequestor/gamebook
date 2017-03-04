bde0a4f5c83e16cf9cfd9d37e2a0b10017a76b3c
Matt Zager <mzager09@gmail.com>
2017-03-04 13:36:51 -0500

    'commitme'

--- js/main.js
+++ js/main.js
@@ -3,51 +3,64 @@ var openingText = "Choose whether the following statements made by Donald Trump 
 document.getElementById("text1").innerHTML = openingText;
 $('#button1').css({display: "block"});
 
+//Show true and false buttons
 $('#button1').click(function(){
-document.getElementById("text1").innerHTML = statement[1].text;
 $(this).hide(function(){
-	$('#button2, #button3').css({display:"block"});
+$('#button2, #button3').css({display:"block"});
+displayStatement();
 });
 });
 
-//constructor for statement objects
-function falseStatement(text,answer){
+//constructor for false statement objects
+function falseStatement(text, answer, value){
 this.text = text;
 this.answer = answer;
-this.accuracy = false;
+this.accuracy = value;
 };
 
 var statement = [];
 
-statement[0] = new falseStatement("'As secretary of state, Hillary Clinton allowed thousands of criminal aliens to be released because their home countries wouldn’t take them back.' ... 10.21.2016", "A 2001 Supreme Court decision required these people to be released if their home countries wouldn’t take them back. While some critics believe the Bush and Obama administration should have done more to pressure these countries, it wasn’t Clinton’s optional decision to release them.");
+statement[0] = new falseStatement("'As secretary of state, Hillary Clinton allowed thousands of criminal aliens to be released because their home countries wouldn’t take them back.' ... 10.21.2016", "A 2001 Supreme Court decision required these people to be released if their home countries wouldn’t take them back. While some critics believe the Bush and Obama administration should have done more to pressure these countries, it wasn’t Clinton’s optional decision to release them.", false);
 
-statement[1] = new falseStatement("Hillary Clinton laughed at a 12-year-old rape victim: 'Her client she represented got him off and she's seen laughing at the girl who was raped.'","Clinton laughed at various points of a 1980s interview about the case – but never at the victim. For example, she said that her client had passed a polygraph lie test, and she added, with a laugh, 'which forever destroyed my faith in polygraphs.'");
+statement[1] = new falseStatement("Hillary Clinton laughed at a 12-year-old rape victim: 'Her client she represented, (she) got him off and she's seen laughing at the girl who was raped.'","Clinton laughed at various points of a 1980s interview about the case – but never at the victim. For example, she said that her client had passed a polygraph lie test, and she added, with a laugh, 'which forever destroyed my faith in polygraphs.'", false);
 
-//START create 6 false states and put in array
+statement[2] = new falseStatement("this is 3","ok",false);
+statement[3] = new falseStatement("this is 4","ok",false);
+statement[4] = new falseStatement("this is 5","ok",false);
+statement[5] = new falseStatement("this is 6","ok", true);
+statement[6] = new falseStatement("this is 7","ok",true);
+statement[7] = new falseStatement("this is 8","ok",true);
+statement[8] = new falseStatement("this is 9","ok",true);
+statement[9] = new falseStatement("this is 10","ok",true);
+//calls randomizer to select a statement
+var i
+var displayStatement = function(){
+i = randomizer();
+document.getElementById("text1").innerHTML = statement[i].text;
+};
 
-//this code sends a Question object to the fact function or alt fact function when button clicked to tell if question was fact or alt fact
-/*
-$("#radio7").click(function(){
-fact(qUestion1);
+$("#button2").click(function(){
+trueTest(true, statement[i].value);
 });
 
-//Tests whether question player answered true to actually is true
-var fact = function(question){
-if(question.accuracy === true){
-}
-};
-///////////////
-*/
-//LAUNCHES GAME, TELLS YOU MAIN PLOT
-function newGame () {
+$("#button3").click(function(){
+trueTest(false, statement[i].value);
+});
 
+//START not getting correct confirmation
+var trueTest = function(answer, question){
+if(this.answer === this.question){
+confirm("that's right");
+}
+else{
+confirm("that's wrong");
+}
 };
 
 var randomizer = function() {
 return Math.floor((Math.random() * 10) + 1);
 };
 
-//newGame();
 });
 
 
